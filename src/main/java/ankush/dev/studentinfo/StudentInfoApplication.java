600b332b379476a878e9a67b787ad690ea0df35f
ankush-m14 <ankushmunde1996@gmail.com>
2024-01-25 18:37:53 +0530

    changes

--- src/main/java/ankush/dev/studentinfo/StudentInfoApplication.java
+++ src/main/java/ankush/dev/studentinfo/StudentInfoApplication.java
@@ -1,13 +1,18 @@
 package ankush.dev.studentinfo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentInfoApplication {
+public class StudentInfoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentInfoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
--- src/main/java/ankush/dev/studentinfo/dto/ExceptionDto.java
+++ src/main/java/ankush/dev/studentinfo/dto/ExceptionDto.java
@@ -1,7 +1,11 @@
 package ankush.dev.studentinfo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@Setter
 public class ExceptionDto {
     private HttpStatus httpStatus;
     private String message;
